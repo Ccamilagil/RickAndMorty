0d5643cd23c0154142b1642e21bca96cf76f8a82
ccamilagil <ccamila.gil.borel@gmail.com>
2023-08-14 11:41:43 -0300

    primer commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from './Card';
+
+export default function Cards(props) {
+   return (
+      <div>
+         {props.characters.map((pj) => (
+            <Card
+               key={pj.id}
+               name={pj.name}
+               species={pj.species}
+               onClose={() => window.alert("Emulacion")}
+               gender={pj.gender}
+               status={pj.status}
+               image={pj.image}
+               origin={pj.origin.name}
+            />
+         ))}
+      </div>
+   );
+}
